2e3ec0a5e08783ff89fcf58a8827e4ae2a50ce89
Raheel Baloch <82443860+SkyLineCoderz-Raheel@users.noreply.github.com>
2024-04-07 22:23:08 +0500

    Booking Section Payment Request Done

--- lib/main.dart
+++ lib/main.dart
@@ -140,7 +140,7 @@ class _MyAppState extends State<MyApp> {
     FlutterLocalNotificationsPlugin();
 // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     const AndroidInitializationSettings initializationSettingsAndroid =
-    AndroidInitializationSettings('@mipmap/ic_launcher');
+    AndroidInitializationSettings('@mipmap/launcher_icon');
     const DarwinInitializationSettings initializationSettingsIOS =
     DarwinInitializationSettings();
     const DarwinInitializationSettings initializationSettingsMacOS =
